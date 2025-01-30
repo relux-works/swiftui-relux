aa1377c475a87d1f053cae2b26a6e442af360698
Ivan <98310998+ivanopcode@users.noreply.github.com>
2025-01-30 21:35:14 +0400

    Update relux router package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ivalx1s/darwin-relux.git", .upToNextMajor(from: "5.2.0")),
-        .package(url: "https://github.com/ivalx1s/swiftui-reluxrouter.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/ivalx1s/swiftui-reluxrouter.git", .upToNextMajor(from: "4.1.0"))
     ],
     targets: [
         .target(
