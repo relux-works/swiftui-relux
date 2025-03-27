bcdca3441b44999e2af5affab46dc46010886281
alexis <alexis.ag85@gmail.com>
2025-03-27 21:23:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ivalx1s/darwin-relux.git", .upToNextMajor(from: "8.0.3")),
-        .package(url: "https://github.com/ivalx1s/swiftui-reluxrouter.git", .upToNextMajor(from: "7.0.1"))
+        .package(url: "https://github.com/ivalx1s/darwin-relux.git", .upToNextMajor(from: "8.0.4")),
+        .package(url: "https://github.com/ivalx1s/swiftui-reluxrouter.git", .upToNextMajor(from: "7.1.0"))
     ],
     targets: [
         .target(
