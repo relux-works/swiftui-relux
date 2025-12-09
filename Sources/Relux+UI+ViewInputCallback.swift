ccb327aec69ee57e94857d20c3f51c3829d4a12b
Ivan Oparin <oparin@me.com>
2025-12-09 22:40:26 +0400

    Refactpr to support generic ViewCallback with parameter packs

--- Sources/Relux+UI+ViewInputCallback.swift
+++ Sources/Relux+UI+ViewInputCallback.swift
@@ -1,21 +1,19 @@
-
 public extension Relux.UI {
-    struct ViewInputCallback<Input: Sendable>: Sendable, Equatable, Identifiable {
+    struct ViewCallback<Input: Sendable>: Sendable, Equatable, Identifiable {
+        
         public struct CallSite: Hashable, Sendable {
             let file: String
             let function: String
             let line: UInt
-
             public init(file: String, function: String, line: UInt) {
-                self.file = file
-                self.function = function
-                self.line = line
+                self.file = file; self.function = function; self.line = line
             }
         }
-
+        
         public let id: CallSite
         private let action: @Sendable (Input) async -> Void
-
+        
+        // Standard Init (for 1..N arguments)
         public init(
             file: String = #fileID,
             function: String = #function,
@@ -25,13 +23,36 @@ public extension Relux.UI {
             self.id = CallSite(file: file, function: function, line: line)
             self.action = action
         }
-
+        
         public func callAsFunction(_ input: Input) async {
             await action(input)
         }
-
+        
         public static func == (lhs: Self, rhs: Self) -> Bool {
             lhs.id == rhs.id
         }
     }
 }
+
+public extension Relux.UI.ViewCallback where Input == Void {
+    init(
+        file: String = #fileID,
+        function: String = #function,
+        line: UInt = #line,
+        _ action: @escaping @Sendable () async -> Void
+    ) {
+        self.id = CallSite(file: file, function: function, line: line)
+        self.action = { _ in await action() }
+    }
+    
+    func callAsFunction() async {
+        await action(())
+    }
+}
+
+public extension Relux.UI.ViewCallback {
+    func callAsFunction<each T>(_ args: repeat each T) async where Input == (repeat each T) {
+        await action((repeat each args))
+    }
+}
+
