68eb205a97d6af114ba3ebc9031b629ecc490e7c
Ivan Oparin <oparin@me.com>
2025-12-09 05:20:26 +0400

    Conform ViewProps to DynamicProperty

--- Sources/Protocols/View+Props.swift
+++ Sources/Protocols/View+Props.swift
@@ -1,3 +1,5 @@
-public protocol ViewProps: Equatable, Hashable, Sendable {}
+import SwiftUI
+
+public protocol ViewProps: DynamicProperty, Equatable, Hashable, Sendable {}
 
 public protocol ViewActions: Sendable {}
