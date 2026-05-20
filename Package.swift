c1480e081a3134899272da8400e8732eeea62c65
Artem Grishchenko <timmi.gri@gmail.com>
2026-05-20 11:32:45 +0300

    ios 15 support without router

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "swiftui-relux",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
+        .iOS(.v15),
+        .macOS(.v12),
     ],
     products: [
         .library(
@@ -14,15 +14,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/relux-works/swift-relux.git", .upToNextMajor(from: "9.0.0")),
-        .package(url: "https://github.com/relux-works/swiftui-reluxrouter.git", .upToNextMajor(from: "11.0.0"))
+        .package(url: "https://github.com/relux-works/swift-relux.git", .upToNextMajor(from: "9.0.0"))
     ],
     targets: [
         .target(
             name: "SwiftUIRelux",
             dependencies: [
                 .product(name: "Relux", package: "swift-relux"),
-                .product(name: "ReluxRouter", package: "swiftui-reluxrouter"),
             ],
             path: "Sources"
         )
--- Sources/Reexported+Modules.swift
+++ Sources/Reexported+Modules.swift
@@ -1,2 +1,1 @@
-@_exported import ReluxRouter
 @_exported import Relux
